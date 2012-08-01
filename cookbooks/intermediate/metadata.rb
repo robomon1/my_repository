6a8540646f3b385680456a8d80544aa27ca916bf
root <root@ip-10-252-15-136.us-west-2.compute.internal>
2012-08-01 23:41:13 +0000

    Added rightscale_marker to default.rb

--- cookbooks/intermediate/metadata.rb
+++ cookbooks/intermediate/metadata.rb
@@ -5,5 +5,7 @@ description      "Installs/Configures intermediate"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "intermediate::default","Prints hello worldd output"
 
