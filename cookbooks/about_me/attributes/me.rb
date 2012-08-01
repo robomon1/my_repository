e66bed54075d10280c34ba2bc5014a0248d092d3
root <root@ip-10-252-15-136.us-west-2.compute.internal>
2012-08-01 23:54:37 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,5 @@
+set[:about_me][:first_name] = "<INSERT_YOUR_NAME_HERE>"
+default[:about_me][:favorite][:food] = "pizza"
+default[:about_me][:favorite][:drink] = "water"
+default[:about_me][:favorite][:hobby] = "running"
+
